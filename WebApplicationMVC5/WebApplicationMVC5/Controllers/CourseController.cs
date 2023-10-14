60234e6a0d543927dacb74956927b2a86cb2a58d
Leticia Pimenta <13leticia.pimentas@gmail.com>
2023-10-14 00:06:48 -0300

    Metodo para listar cursos

---
+++ WebApplicationMVC5/WebApplicationMVC5/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationMVC5.Models;
+
+namespace WebApplicationMVC5.Controllers
+{
+    public class CourseController : Controller
+    {
+        #region Method for Listing Courses
+        public JsonResult GetCourse()
+        {
+            using (var db = new CoursesEntities())
+            {
+                List<Courses> listingCourses = db.Courses.ToList();
+
+                return Json(listingCourses);
+            }
+        }
+
+        #endregion
+    }
+}
