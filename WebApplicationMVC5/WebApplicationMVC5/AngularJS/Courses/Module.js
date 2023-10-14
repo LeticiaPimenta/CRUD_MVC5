d80c88219fc556627d6dc07f7132245aad7c7b6b
Leticia Pimenta <13leticia.pimentas@gmail.com>
2023-10-14 00:46:16 -0300

    Created 'Courses' module

---
+++ WebApplicationMVC5/WebApplicationMVC5/AngularJS/Courses/Module.js
@@ -0,0 +1,19 @@
+﻿/**
+  * File: Module.js
+  * Date: 10/14/2023
+  * Description: This file will contain the 'Courses' module, which we will use to be able to
+  * perform CRUD operations through ng-app in the view part of the application
+  * Author: Leticia Piment
+  */
+
+var coursesApp;
+
+(function () {
+
+    courseApp = angular.module('courses', []);
+    disciplineApp = angular.module('disciplines', []);
+    gradeApp = angular.module('grades', []);
+    professorApp = angular.module('professors', []);
+    studentApp = angular.module('students', []);
+
+})();
